926dcc98673ae479965a7af5a81667952bae5c3b
Gabriel Halley <gabhalley@gmail.com>
2015-12-23 20:00:48 -0500

    day 2 part 2 solution

--- day_2.rb
+++ day_2.rb
@@ -1013,11 +1013,19 @@ def wrapping_paper(l, w, h)
   total + extra
 end
 
+def get_smaller_sides(sides)
+  sides.first*2 + sides.last*2
+end
+
 total_sq_footage = 0
+total_ribbon_footage = 0
 
 presents.each do |present|
   dimensions = present.split('x').map {|d| d.to_i}
   total_sq_footage += wrapping_paper(dimensions[0].to_i, dimensions[1].to_i, dimensions[2]).to_i
+  total_ribbon_footage += get_smaller_sides(dimensions.sort[0..1]) + dimensions.reduce(:*)
 end
 
 puts total_sq_footage
+puts total_ribbon_footage
+
