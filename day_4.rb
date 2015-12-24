cea20493ab0fe103112a380f5a9ea9875c492f0f
Gabriel Halley <gabhalley@gmail.com>
2015-12-23 22:03:33 -0500

    day 4 part 2 solution

--- day_4.rb
+++ day_4.rb
@@ -1,6 +1,6 @@
 require 'digest'
 
-def find_md5(input)
+def find_md5(input, zeroes)
   not_found = true
   number = 0
   while not_found
@@ -8,11 +8,12 @@ def find_md5(input)
     md5.update(input)
     md5.update(number.to_s)
 
-    not_found = md5.to_s[0,5] != '00000'
+    not_found = md5.to_s[0,zeroes] != (zeroes==5 ? '00000' : '000000')
     number += 1 if not_found
   end
 
   number
 end
 
-puts find_md5('ckczppom')
+puts find_md5('ckczppom', 5)
+puts find_md5('ckczppom', 6)
