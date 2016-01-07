75b6dae9ed07115da8d38f468bafdaf222f48b2c
Gabriel Halley <gabhalley@gmail.com>
2016-01-07 07:33:41 -0500

    day 8 part 2 solution!

--- day_8.rb
+++ day_8.rb
@@ -8,3 +8,15 @@ data.each do |line|
 end
 
 puts code_length - mem_length
+
+# Part 2
+
+code_length = 0
+mem_length = 0
+data.each do |line|
+  l = line.chomp
+  code_length += l.length
+  mem_length += l.dump.length
+end
+
+puts mem_length - code_length
