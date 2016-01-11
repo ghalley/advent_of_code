fb4257c3cd88114e2cb5527b20b2fd9cb4f3ea91
Gabriel Halley <ghalley@pindropsecurity.com>
2016-01-11 11:30:22 -0500

    day 10 part 1 solution

--- day_10.rb
+++ day_10.rb
@@ -1,25 +1,28 @@
 def count_digits(string)
-  new_array = []
-  current_count = 1
+  40.times do
+    new_array = []
+    current_count = 1
 
-  string.split('').each_with_index do |char, index|
-    next if index == 0
-    if char == string[index-1]
-      current_count+=1
-    else
-      new_array << [current_count.to_s, string[index-1]]
-      current_count = 1
+    string.split('').each_with_index do |char, index|
+      next if index == 0
+      if char == string[index-1]
+        current_count+=1
+      else
+        new_array << [current_count.to_s, string[index-1]]
+        current_count = 1
+      end
+
+      if index == (string.length - 1) && char == string[index-1]
+        new_array << [current_count.to_s, char]
+      elsif index == (string.length - 1)
+        new_array << [1, char]
+      end
+
     end
 
-    if index == (string.length - 1) && char == string[index-1]
-      new_array << [current_count.to_s, char]
-    elsif index == (string.length - 1)
-      new_array << [1, char]
-    end
-
+    string = new_array.flatten.join
   end
-
-  puts "#{new_array.flatten.join.length}"
+  puts string.length
 end
 
 count_digits('1113222113')
