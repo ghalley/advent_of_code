7f81b298c84818119c56d2c01ec72e61781ec3cc
Gabriel Halley <ghalley@pindropsecurity.com>
2016-01-12 12:38:00 -0500

    day 13 part 1 solution

--- day_13.rb
+++ day_13.rb
@@ -1,11 +1,30 @@
+def calculate_happines(name1, name2, happy_hash)
+  name1_total = happy_hash[name1][name2]
+  name2_total = happy_hash[name2][name1]
+  name1_total + name2_total
+end
+
 file = File.readlines('inputs/day_13.txt')
 happy_hash = {}
 file.each do |line|
+  line.gsub!('.', '')
   line = line.split(' ')
+  sign = line[2] == 'gain' ? 1 : -1
   if happy_hash[line.first].nil?
-    happy_hash[line.first] = {line.last => {operation: line[2], amount: line[3]}}
+    happy_hash[line.first] = {line.last => (line[3].to_i * sign)}
   else
-    happy_hash[line.first][line.last] = {operation: line[2], amount: line[3]}
+    happy_hash[line.first][line.last] = (line[3].to_i * sign)
   end
 end
-puts happy_hash
+
+potential_arrangements = {}
+
+happy_hash.keys.permutation.to_a.each do |order|
+  total = 0
+  order.each_with_index do |name, index|
+    total = total + calculate_happines(name, order[index-1], happy_hash)
+  end
+  potential_arrangements[order] = total
+end
+
+puts "#{potential_arrangements.values.sort}"
