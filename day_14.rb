b728709679937c5c9d3f0101569a5aa6f93263a1
Gabriel Halley <ghalley@pindropsecurity.com>
2016-01-12 15:15:12 -0500

    day 14 data to hash

---
+++ day_14.rb
@@ -0,0 +1,10 @@
+source_data = [{name: 'Dancer', rate: 27, duration: 5, recovery: 132}
+{name: 'Cupid', rate: 22, duration: 2, recovery: 41}
+{name: 'Rudolph', rate: 11, duration: 5, recovery: 48}
+{name: 'Donner', rate: 28, duration: 5, recovery: 134}
+{name: 'Dasher', rate: 4, duration: 16, recovery: 55}
+{name: 'Blitzen', rate: 14, duration: 3, recovery: 38}
+{name: 'Prancer', rate: 3, duration: 21, recovery: 40}
+{name: 'Comet', rate: 18, duration: 6, recovery: 103}
+{name: 'Vixen', rate: 18, duration: 5, recovery: 84}]
+
