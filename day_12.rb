4f44b31cb5a5b75b1f822e43a4ea6374230e5e31
Gabriel Halley <gabhalley@gmail.com>
2016-01-11 23:21:31 -0500

    day 12 part 2 solution

--- day_12.rb
+++ day_12.rb
@@ -7,11 +7,11 @@ json = JSON.parse(file)
 # if hash, traverse with each_value
 # if value is not hash or array, and is fixnum, add to sum
 
-def traverse_json(item)
+def traverse_json(item, ignored=nil)
   if item.is_a? Array
-    item.map {|i| traverse_json(i)}.reduce(:+)
+    item.map {|i| traverse_json(i, ignored)}.reduce(:+)
   elsif item.is_a? Hash
-    traverse_json(item.values)
+    item.values.include?(ignored) ? 0 : traverse_json(item.values, ignored)
   elsif item.is_a? Fixnum
     item
   else
@@ -20,9 +20,12 @@ def traverse_json(item)
 end
 
 current_sum = 0
+ignored_sum = 0
 
 json.each do |item|
   current_sum += traverse_json(item)
+  ignored_sum += traverse_json(item, 'red')
 end
 
 puts current_sum
+puts ignored_sum
