d4ac52c6048c5a66f0f0c25cdbf7957e46007a9c
Gabriel Halley <ghalley@pindropsecurity.com>
2016-01-19 11:16:04 -0500

    day 16 part 2 solution

--- day_16.rb
+++ day_16.rb
@@ -24,6 +24,26 @@ def compare_sue(sue, present)
   present.merge(new_sue) == present
 end
 
+def new_compare_sue(sue, present)
+  new_sue = sue.dup
+
+  return false if new_sue['cats'] && new_sue['cats'] < present['cats']
+  return false if new_sue['trees'] && new_sue['trees'] < present['trees']
+  return false if new_sue['pomeranians'] && new_sue['pomeranians'] > present['pomeranians']
+  return false if new_sue['goldfish'] && new_sue['goldfish'] > present['goldfish']
+
+  new_sue.delete(:id)
+  new_sue.delete('cats')
+  new_sue.delete('trees')
+  new_sue.delete('pomeranians')
+  new_sue.delete('goldfish')
+
+  present.merge(new_sue) == present
+end
+
+second_sues = sues.map {|s| s if new_compare_sue(s, clues)}
+
 sues.keep_if {|s| compare_sue(s, clues)}
 
 puts "#{sues.first[:id]}"
+puts "#{second_sues.compact.first[:id]}"
