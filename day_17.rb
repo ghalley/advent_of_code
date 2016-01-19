eb5af37ec024755fcf25da8e290f6bc62b00c602
Gabriel Halley <ghalley@pindropsecurity.com>
2016-01-19 14:26:22 -0500

    day 17 part 2 solution

--- day_17.rb
+++ day_17.rb
@@ -1,16 +1,17 @@
 container_input = [33, 14, 18, 20, 45, 35, 16, 35, 1, 13, 18, 13, 50, 44, 48, 6, 24, 41, 30, 42]
 
-# puts "#{}"
-
 def fill_containers(permutation)
   permutation
 end
 
-all_containers = 0
+all_containers = []
 container_input.size.times do |t|
   container_input.combination(t).each do |p|
-    all_containers += 1 if p.reduce(:+) == 150
+    all_containers << p if p.reduce(:+) == 150
   end
 end
 
-puts "#{all_containers}"
+puts "#{all_containers.length}"
+
+min_length = all_containers.sort_by {|c| c.length}.first.length
+puts "#{all_containers.keep_if {|c| c.length == min_length}.length}"
