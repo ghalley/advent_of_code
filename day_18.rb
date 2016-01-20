5621aa12fc6cffd3e3dbb0da8af08df3334221a4
Gabriel Halley <ghalley@pindropsecurity.com>
2016-01-20 11:01:00 -0500

    take steps correctly

--- day_18.rb
+++ day_18.rb
@@ -9,21 +9,6 @@ sample = '.#.#.#
 puts sample
 # puts `clear`
 
-lights = []
-sample.split("\n").each do |line|
-  lights << line.split('')
-end
-
-def step_grid(grid)
-  copy_grid = grid.dup
-
-  grid.each do |row|
-    row.each do |column|
-      # check neighbors
-    end
-  end
-end
-
 def check_neighbors(light, row, column, grid)
   neighbors_on = 0
   neighbors_on += 1 if ((row - 1) >= 0 && (column - 1) >= 0) && grid[row - 1][column - 1] == '#'
@@ -35,6 +20,42 @@ def check_neighbors(light, row, column, grid)
   neighbors_on += 1 if (row < (grid.length - 1)) && grid[row + 1][column] == '#'
   neighbors_on += 1 if (row < (grid.length - 1)) && column < grid[row+1].length && grid[row + 1][column + 1] == '#'
 
-  neighbors_on
+  if light == '#' && [2,3].include?(neighbors_on)
+    '#'
+  elsif light == '#'
+    '.'
+  elsif light == '.' && neighbors_on == 3
+    '#'
+  else
+    '.'
+  end
 end
 
+def step_grid(grid)
+  copy_grid = []
+
+  grid.length.times do |row|
+    new_row = []
+
+    grid[row].length.times do |column|
+      new_row << check_neighbors(grid[row][column], row, column, grid)
+    end
+    copy_grid << new_row
+  end
+  copy_grid
+end
+
+lights = []
+sample.split("\n").each do |line|
+  lights << line.split('')
+end
+
+grid = lights
+100.times do
+  grid = step_grid(grid)
+end
+
+puts
+grid.each do |row|
+  puts row.join('')
+end
