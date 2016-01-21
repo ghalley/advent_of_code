03205abd8470721923aa77d2c519c0891024b859
Gabriel Halley <ghalley@pindropsecurity.com>
2016-01-21 13:02:54 -0500

    day 19 first pass

--- day_19.rb
+++ day_19.rb
@@ -1,5 +1,21 @@
 input = File.read('inputs/day_19.txt').split("\n")
 
+def find_indices(target, string)
+  index = 0
+  indices = []
+  while index = string.index(target,index)
+    indices << index
+    index += 1
+  end
+  indices
+end
+
+def swap_string(orignal_string, replacement_value, index)
+  new_string = orignal_string.split('')
+  new_string[index] = replacement_value
+  new_string.join('')
+end
+
 replacement_keys = []
 
 original_molecule = input.last
@@ -9,4 +25,21 @@ input[0..-3].each do |line|
   replacement_keys << {pieces.first => pieces.last}
 end
 
-puts replacement_keys
+
+possibilities = []
+
+# sample stuff
+# replacement_keys = [{'H' => 'HO'}, {'H' => 'OH'}, {'O' => 'HH'}]
+# original_molecule = 'HOH'
+
+replacement_keys.each do |key|
+  find_indices(key.keys.first, original_molecule).each do |index|
+    string = original_molecule
+    possibilities << swap_string(original_molecule, key.values.first, index)
+  end
+end
+
+puts possibilities.uniq.count
+
+# puts "#{swap_string('HOH', 'HO', 0)}"
+# find_indices('H', 'HOH')
