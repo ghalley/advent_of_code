c767c8c684ca57ef9ce1dd16c1b7366477d90ec4
Gabriel Halley <ghalley@pindropsecurity.com>
2016-02-03 15:10:38 -0500

    day 20 part 2 solution

--- day_20.rb
+++ day_20.rb
@@ -4,7 +4,7 @@ def find_divisors(original_number)
   divisors = []
   1.upto((original_number.to_f**0.5).floor).each do |current_num|
     q,r = original_number.divmod current_num
-    divisors << q << r if r.zero?
+    divisors << q if r.zero?
   end
   divisors.uniq.sort
 end
@@ -23,4 +23,19 @@ def find_house(minimum)
   index
 end
 
+def find_another_house(minimum)
+  index = 1
+  found = false
+  while !found
+    index +=1
+    next if Prime.prime?(index)
+    divisors = find_divisors(index)
+
+    sum = divisors.map! { |d| index/d <= 50 ? (d * 11) : 0 }
+    # puts "#{sum}"
+    found = sum.reduce(:+) >= minimum
+  end
+  index
+end
 puts find_house(36000000)
+puts find_another_house(36000000)
