a50574fe74d01164ebd45f0409c201461eab37dc
Gabriel Halley <ghalley@pindropsecurity.com>
2016-02-03 13:50:11 -0500

    day 20 part 1 solution

--- day_20.rb
+++ day_20.rb
@@ -1,11 +1,26 @@
+require "prime"
+
 def find_divisors(original_number)
   divisors = []
-  (1..original_number).to_a.each do |current_num|
-    if original_number % current_num == 0
-      divisors << current_num
-    end
+  1.upto((original_number.to_f**0.5).floor).each do |current_num|
+    q,r = original_number.divmod current_num
+    divisors << q << r if r.zero?
   end
-  divisors
+  divisors.uniq.sort
 end
 
-find_divisors(9)
+def find_house(minimum)
+  index = 310000
+  found = false
+  while !found
+    index +=1
+    next if Prime.prime?(index)
+    divisors = find_divisors(index)
+
+    sum = divisors.map! { |d| d * 10 }.reduce(:+)
+    found = sum >= minimum
+  end
+  index
+end
+
+puts find_house(36000000)
