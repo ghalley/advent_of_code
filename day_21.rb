fa3977e1d687e8a799fcf5f7a59b6285edb5f813
Gabriel Halley <ghalley@pindropsecurity.com>
2016-02-04 16:29:57 -0500

    day 21 armor/weapon data

---
+++ day_21.rb
@@ -0,0 +1,19 @@
+weapons = [{name: 'Dagger', damage: 4, cost: 8},
+           {name: 'Shortsword', damage: 5, cost: 10},
+           {name: 'Warhammer', damage: 6, cost: 25},
+           {name: 'Longsword', damage: 7, cost: 40},
+           {name: 'Greataxe', damage: 8, cost: 74},
+           {name: 'Ring+1', damage: 1, cost: 25},
+           {name: 'Ring+2', damage: 2, cost: 50},
+           {name: 'Ring+3', damage: 3, cost: 100}
+]
+
+armor =   [{name: 'Leather', armor: 1, cost: 13},
+           {name: 'Chainmail', armor: 2, cost: 31},
+           {name: 'Splintmail', armor: 3, cost: 53},
+           {name: 'Bandedmail', armor: 4, cost: 75},
+           {name: 'Greataxe', armor: 5, cost: 102},
+           {name: 'Ring+1', armor: 1, cost: 20},
+           {name: 'Ring+2', armor: 2, cost: 40},
+           {name: 'Ring+3', armor: 3, cost: 80}
+]
