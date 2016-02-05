a3797a84b5233edb49066fcab651a69e5d671ee6
Gabriel Halley <ghalley@pindropsecurity.com>
2016-02-05 15:34:04 -0500

    day 21 part 2 solution

--- day_21.rb
+++ day_21.rb
@@ -5,7 +5,8 @@ weapons = [{name: 'Dagger', damage: 4, cost: 8},
            {name: 'Greataxe', damage: 8, cost: 74}
 ]
 
-armor =   [{name: 'Leather', armor: 1, cost: 13},
+armor =   [{name: 'none', armor: 0, cost: 0},
+           {name: 'Leather', armor: 1, cost: 13},
            {name: 'Chainmail', armor: 2, cost: 31},
            {name: 'Splintmail', armor: 3, cost: 53},
            {name: 'Bandedmail', armor: 4, cost: 75},
@@ -22,6 +23,7 @@ rings = [{cost: 0, damage: 0, armor: 0},
 ]
 
 gold_spent = []
+gold_wasted = []
 
 weapons.each do |weapon|
   armor.each do |armor|
@@ -32,16 +34,19 @@ weapons.each do |weapon|
       damage = weapon[:damage] + ring1[:damage] + ring2[:damage]
       defense = armor[:armor] + ring1[:armor] + ring2[:armor]
       hit_points = 100
+      total_cost = weapon[:cost] + armor[:cost] + ring1[:cost] + ring2[:cost]
 
       while hit_points >0 && boss_hp > 0
-        boss_hp -= (damage - boss_armor)
-        break if boss_hp <= 0
-        hit_points -= (boss_damage - defense)
+        boss_hp -= [1, damage-boss_armor].max
+        break if boss_hp <= 0 || hit_points <=0
+        hit_points -= [1, boss_damage-defense].max
       end
 
-      gold_spent << (weapon[:cost] + armor[:cost] + ring1[:cost] + ring2[:cost]) if boss_hp <= 0
+      gold_spent << total_cost if boss_hp <= 0
+      gold_wasted << total_cost if hit_points <= 0 && boss_hp > 0
     end
   end
 end
 
 puts "#{gold_spent.min}"
+puts "#{gold_wasted.max}"
