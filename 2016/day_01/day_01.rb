3ce05cb26b1cd85cf8730688b6d84ae69b861378
Gabriel Halley <ghalley@pindropsecurity.com>
2016-12-21 16:05:27 -0500

    day 2 part 2

--- 2016/day_01/day_01.rb
+++ 2016/day_01/day_01.rb
@@ -1,9 +1,9 @@
 require 'pry'
 class Day01
-  EAST = {axis: 'y', L: '+', R: '-'}
-  NORTH = {axis: 'x', L: '-', R: '+'}
-  WEST = {axis: 'y', L: '-', R: '+'}
-  SOUTH = {axis: 'x', L: '+', R: '-'}
+  EAST = {axis: 'y', L: :+, R: :-}
+  NORTH = {axis: 'x', L: :-, R: :+}
+  WEST = {axis: 'y', L: :-, R: :+}
+  SOUTH = {axis: 'x', L: :+, R: :-}
 
   def self.run(instructions)
     directions = instructions.split(', ')
@@ -15,6 +15,23 @@ class Day01
     end
 
     @coordinates.map {|xy| xy.abs}.reduce(:+)
+  end
+
+  def self.run2(instructions)
+    directions = instructions.split(', ')
+    @coordinates = [0,0]
+    @facing = NORTH
+    path = []
+    directions.each do |direction|
+      old = @coordinates.dup
+      move(direction)
+      orientation(direction)
+      path << find_line(old, @coordinates)
+    end
+
+    path << [@coordinates]
+    path.flatten!(1)
+    path.keep_if { |c| path.index(c) != path.rindex(c)}.uniq.first.flatten.map {|xy| xy.abs}.reduce(:+)
   end
 
   def self.orientation(direction)
@@ -32,9 +49,28 @@ class Day01
 
   def self.move(direction)
     if @facing[:axis] == 'x'
-      @coordinates[0] = @coordinates.first.send(@facing[direction[0].to_sym].to_sym, direction[1..-1].to_i)
+      @coordinates[0] = @coordinates.first.send(@facing[direction[0].to_sym], direction[1..-1].to_i)
     else
-      @coordinates[1] = @coordinates.last.send(@facing[direction[0].to_sym].to_sym, direction[1..-1].to_i)
+      @coordinates[1] = @coordinates.last.send(@facing[direction[0].to_sym], direction[1..-1].to_i)
     end
+  end
+
+  def self.find_line(start, finish)
+    line = [start]
+
+    if start.first == finish.first
+      distance = (start.last - finish.last)
+      (distance.abs - 1).times do |t|
+        new_coord = distance > 0 ? line.last.last - 1 : line.last.last + 1
+        line << [start.first, new_coord]
+      end
+    else
+      distance = (start.first - finish.first)
+      (distance.abs - 1).times do |t|
+        new_coord = distance > 0 ? line.last.first - 1 : line.last.first + 1
+        line << [new_coord, start.last]
+      end
+    end
+    line
   end
 end
--- 2016/day_01/day_01_test.rb
+++ 2016/day_01/day_01_test.rb
@@ -7,33 +7,56 @@ require_relative 'day_01'
 Dir.chdir(File.dirname(__FILE__))
 
 describe "Day 01" do
-  it 'should caculate the first example' do
-    instructions = 'R2, L3'
+  describe 'Part 1' do
+    it 'should caculate the first example' do
+      instructions = 'R2, L3'
 
-    assert_equal 5, Day01.run(instructions)
+      assert_equal 5, Day01.run(instructions)
+    end
+
+    it 'should calculate the second example' do
+      instructions = 'R2, R2, R2'
+
+      assert_equal 2, Day01.run(instructions)
+    end
+
+    it 'a full circle as 0' do
+      instructions = 'R2, R2, R2, R2'
+
+      assert_equal 0, Day01.run(instructions)
+    end
+
+    it 'should calculate the third example' do
+      instructions = 'R5, L5, R5, R3'
+
+      assert_equal 12, Day01.run(instructions)
+    end
+
+    it 'calculates the output' do
+      instructions = File.read('day01-1.txt')
+
+      assert_equal 239, Day01.run(instructions)
+    end
   end
 
-  it 'should calculate the second example' do
-    instructions = 'R2, R2, R2'
+  describe 'Part 2' do
+    it 'finds the first repeated location' do
+      instructions = 'R8, R4, R4, R8'
 
-    assert_equal 2, Day01.run(instructions)
-  end
+      assert_equal 4, Day01.run2(instructions)
+    end
 
-  it 'a full circle as 0' do
-    instructions = 'R2, R2, R2, R2'
+    it 'calculates the coordinates in a path' do
+      coordinates1 = [0,0]
+      coordinates2 = [0,3]
+      assert_instance_of Array, Day01.find_line(coordinates1, coordinates2)
+      assert_equal [[0,0], [0,1], [0,2]], Day01.find_line(coordinates1, coordinates2)
+    end
 
-    assert_equal 0, Day01.run(instructions)
-  end
+    it 'calculates the output' do
+      instructions = File.read('day01-1.txt')
 
-  it 'should calculate the third example' do
-    instructions = 'R5, L5, R5, R3'
-
-    assert_equal 12, Day01.run(instructions)
-  end
-
-  it 'calculates the output' do
-    instructions = File.read('day01-1.txt')
-
-    assert_equal 239, Day01.run(instructions)
+      assert_equal 141, Day01.run2(instructions)
+    end
   end
 end
