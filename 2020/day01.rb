ac7b7acc4bda86bf55b791b14a117b3e854b59b6
Gabriel Halley <gabriel.halley@omadahealth.com>
2020-12-04 16:12:25 -0500

    2020 Day 1 pt2

--- 2020/day01.rb
+++ 2020/day01.rb
@@ -1,11 +1,15 @@
-numbers = File.read('inputs/day01.txt').split("\n")
+numbers = File.read('inputs/day01.txt').split("\n").map(&:to_i)
 matching_entries = Array.new(2)
 
 numbers.each do |number|
   next if matching_entries.entries.all?
 
-  matching_entries[0] = number.to_i
-  matches = numbers.select { |n| (matching_entries[0] + n.to_i) == 2020 }
-  matching_entries[1] = matches.first.to_i unless matches.first.nil?
+  matching_entries[0] = number
+  matches = numbers.select { |n| (matching_entries[0] + n) == 2020 }
+  matching_entries[1] = matches.first unless matches.first.nil?
 end
 puts matching_entries.reduce(:*)
+
+combos=numbers.permutation(3).to_a.select {|p| p.reduce(:+) == 2020}
+
+puts combos.first.reduce(:*)
