c9389d3ea62d0c43e22500d0d01de36bf837c45f
Gabriel Halley <gabriel.halley@omadahealth.com>
2020-12-04 17:33:31 -0500

    2020 Day 2 pt2

--- 2020/day02.rb
+++ 2020/day02.rb
@@ -10,3 +10,23 @@ passwords.each do |entry|
 end
 
 puts valid_passwords
+
+valid_passwords = 0
+passwords.each do |entry|
+  indices, letter, password = entry.split(' ')
+  letter.gsub!(':', '')
+
+  indices = indices.split('-').map(&:to_i)
+  next if password.index(letter).nil?
+
+  first = password[indices.first - 1]
+  second = password[indices.last - 1]
+
+  next if first == letter && first == second
+
+  if (password[indices.first - 1] == letter || !password[indices.last - 1] == letter) || (!password[indices.first - 1] == letter || password[indices.last - 1] == letter)
+    valid_passwords += 1
+  end
+end
+
+puts valid_passwords
