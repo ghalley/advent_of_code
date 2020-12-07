843025b4587467578c77ad616494cedb841e4f70
Gabriel Halley <gabriel.halley@omadahealth.com>
2020-12-07 10:16:46 -0500

    2020 Day 3 pt2

--- 2020/day03.rb
+++ 2020/day03.rb
@@ -1,15 +1,19 @@
-path = File.read('inputs/day03.txt').split("\n")
+def count_trees(horizontal, vertical)
+  path = File.read('inputs/day03.txt').split("\n")
+  slope = [horizontal,vertical]
 
-current_index = 0
-current_row = 1
-trees = 0
+  current_index = 0
+  current_row = slope.last
+  trees = 0
 
-# puts path[3][30]
-while current_row < path.length
-  current_index += 3
+  while current_row < path.length
+    current_index += slope.first
 
-  trees += 1 if path[current_row][current_index%31] == '#'
+    trees += 1 if path[current_row][current_index % 31] == '#'
 
-  current_row += 1
+    current_row += slope.last
+  end
+  trees
 end
-puts trees
+
+puts [count_trees(1, 1), count_trees(3, 1), count_trees(5, 1), count_trees(7, 1), count_trees(1, 2)].reduce(:*)
