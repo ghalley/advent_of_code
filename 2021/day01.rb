ab041f0728e731e12b89ae2fcd2baa9b903f3b03
Gabriel Halley <gabhalley@gmail.com>
2021-12-30 11:48:55 -0500

    2021 Day 1-2

--- 2021/day01.rb
+++ 2021/day01.rb
@@ -1,11 +1,29 @@
 depth_readings = File.read('2021/inputs/day01pt1.txt').split("\n").map(&:to_i)
 
-increases = 0
+def track_increases(array)
+  increases = 0
 
-depth_readings.each_with_index do |reading, index|
-  next if index == 0
+  array.each_with_index do |reading, index|
+    next if index == 0
 
-  increases += 1 if reading > depth_readings[index -1]
+    increases += 1 if reading > array[index -1]
+  end
+
+  increases
 end
 
-puts increases
+puts track_increases(depth_readings)
+
+# Part 2
+
+windows = []
+
+depth_readings.each_with_index do |reading, index|
+  windows_index = (index)%8
+  next if depth_readings.length - index < 3
+
+  sum = depth_readings[index, 3].reduce(:+)
+  windows << sum
+end
+
+puts track_increases(windows)
